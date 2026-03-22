69296345ae06b26ae25c301b4ccfb2fdaddde281
abhimanyu8829 <abhimanyucentral@gmail.com>
2026-03-23 00:05:56 +0530

    fix prettier formatting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -6,22 +6,22 @@ app.use(express.json())
 
 let todos = []
 
-app.get("/", (req, res) => {
-  res.json({ message: "Todo App Running!", total: todos.length })
+app.get('/', (req, res) => {
+  res.json({ message: 'Todo App Running!', total: todos.length })
 })
 
-app.get("/todos", (req, res) => {
+app.get('/todos', (req, res) => {
   res.json(todos)
 })
 
-app.post("/todos", (req, res) => {
+app.post('/todos', (req, res) => {
   const todo = { id: Date.now(), text: req.body.text, done: false }
   todos.push(todo)
   res.json(todo)
 })
 
-app.get("/health", (req, res) => {
-  res.status(200).send("OK")
+app.get('/health', (req, res) => {
+  res.status(200).send('OK')
 })
 
-app.listen(PORT, () => console.log("Running on port " + PORT))
+app.listen(PORT, () => console.log('Running on port ' + PORT))
